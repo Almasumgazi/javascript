7c2b5df6aae6a63a699327d078ad9d3a5f5686c7
Almasumgazi <almasumgazi18@gmail.com>
2020-09-13 21:11:04 +0530

    2

--- map.js
+++ map.js
@@ -1,10 +1,36 @@
-const numbers = [1, 2, 3, 4, 5]
+// const numbers = [1, 2, 3, 4, 5]
 
-const number=numbers.map(double)
+// const number=numbers.map(double)
 
-function double(value) {
-    return value *value
-}
+// function double(value) {
+//     return value *value
+// }
 
-console.log(numbers)
-console.log(number);
+// console.log(numbers)
+// console.log(number);
+
+const Product = [
+    {
+        name: 'p1',
+        price: 2312,
+        count: 56
+    },
+    {
+        name: 'p2',
+        price: 6541,
+        count: 456
+    },
+    {
+        name: 'p3',
+        price:1321,
+        count: 465
+    }
+]
+
+
+const TotalPrice = Product.map(item => ({
+    name:item.name,
+    TotalPrice: item.price * item.count
+}))
+
+console.log(TotalPrice)
